aac2a3d99d3ad44e66f7d630a9c84cbf121d291d
Sahil Udar <udarsahil0@gmail.com>
2025-01-09 22:32:33 +0530

    finally some structuring

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Link href="/sign-in">Sign-In Page</Link>
+      <Link href="/preferences">prefrences</Link>
+    </View>
+  );
+}
