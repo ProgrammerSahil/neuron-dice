6b970dda404e0e12567826f07f625c8c7ddd8bc6
Sahil Udar <udarsahil0@gmail.com>
2025-01-10 13:59:09 +0530

    configured nativewind

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,8 +10,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Link href="/sign-in">Sign-In Page</Link>
-      <Link href="/preferences">prefrences</Link>
+      <Link className="text-4xl p-2" href="/sign-in">Sign-In Page</Link>
+      <Link className="text-4xl p-2" href="/preferences">prefrences</Link>
     </View>
   );
 }
