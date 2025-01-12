73b32f6ecac80bec4647db81b4ffb34e10b2f606
Sahil Udar <udarsahil0@gmail.com>
2025-01-12 17:33:45 +0530

    added background image

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,24 +1,31 @@
-import { Text, View } from "react-native";
+import { Text, View, ImageBackground } from "react-native";
 import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View className="flex-1 bg-background-main">
-      {/* Title Section */}
-      <Text className="text-6xl font-montserrat color-text-primary text-center mt-8">
-        Neuron Dice
-      </Text>
-
-      {/* Links Section */}
-      <View className="flex-1 justify-center items-center">
-        <Link
-          className="text-4xl font-montserrat bg-button-background color-button-text px-4 py-2 rounded-lg mb-4"
-          href="/sign-in"
-        >
-          Sign-In Page
-        </Link>
-        
+    <ImageBackground 
+      source={require('../../../assets/images/background.jpg')} 
+      className="flex-1" 
+      resizeMode="cover"
+    >
+      {/* Dark overlay */}
+      <View className="absolute inset-0 bg-black/70" />
+      
+      <View className="flex-1">
+        {/* Title Section */}
+        <Text className="text-6xl font-montserrat color-text-primary text-center mt-8 relative z-10">
+          Neuron Dice
+        </Text>
+        {/* Links Section */}
+        <View className="flex-1 justify-center items-center relative z-10">
+          <Link
+            className="text-4xl font-montserrat bg-button-background color-button-text px-4 py-2 rounded-lg mb-4"
+            href="/sign-in"
+          >
+            Sign-In Page
+          </Link>
+        </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 }
