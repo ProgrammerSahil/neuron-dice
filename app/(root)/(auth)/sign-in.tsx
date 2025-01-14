252dcf351b19084ba3e130c24039c1029a0eb7fa
Sahil Udar <udarsahil0@gmail.com>
2025-01-14 13:04:38 +0530

    started with the signin page

--- app/(root)/(auth)/sign-in.tsx
+++ app/(root)/(auth)/sign-in.tsx
@@ -1,11 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <ImageBackground
+    source={require('../../../assets/images/signinbackground.jpg')}
+    resizeMode="cover"
+      blurRadius={4} 
+    >
+    <SafeAreaView className="h-full">
+
+      <Text className='color-text-primary text-center mt-20 text-5xl font-josefinSans font-semibold'>Sign In</Text>
+    </SafeAreaView>
+    </ImageBackground>
   )
 }
 
