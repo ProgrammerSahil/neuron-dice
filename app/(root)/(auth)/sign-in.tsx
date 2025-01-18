45f8b4912e9be76aa180062878bc7927287d1e02
Sahil Udar <udarsahil0@gmail.com>
2025-01-18 20:50:56 +0530

    login page has button now

--- app/(root)/(auth)/sign-in.tsx
+++ app/(root)/(auth)/sign-in.tsx
@@ -1,8 +1,11 @@
-import { View, Text, ImageBackground } from 'react-native'
+import { View, Text, ImageBackground, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const SignIn = () => {
+  const handleLogin = () => {
+
+  }
   return (
     <ImageBackground
     source={require('../../../assets/images/signinbackground.jpg')}
@@ -14,6 +17,14 @@ const SignIn = () => {
       <Text className='color-text-primary text-center mt-20 text-5xl font-josefinSans font-bold'>Sign In</Text>
       <Text className="text-center mt-14 color-text-secondary font-josefinSans text-3xl ">Interesting articles for you{"\n"}</Text>
       <Text className='text-center color-text-secondary font-josefinSans text-3xl'>From all over the <Text className='font-bold'>internet</Text></Text>
+
+      <TouchableOpacity onPress={handleLogin} className='bg-white shadow-md shadow-zinc-300 rounded-full  py-4 mt-20 mx-6'>
+        <View className='flex flex-row items-center justify-center'>
+        <Image src={"https://cdn-teams-slug.flaticon.com/google.jpg"} className="w-9 h-9 mr-2" resizeMode='contain' />
+          <Text className='text-xl font-montserrat font-bold'>Continue with Google</Text>
+        </View>
+      </TouchableOpacity>
+
     </SafeAreaView>
     </ImageBackground>
   )
