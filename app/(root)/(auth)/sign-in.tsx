8fd566bbcb6b43a11936b93261a3585d44424de0
Sahil Udar <udarsahil0@gmail.com>
2025-01-18 20:54:10 +0530

    will setup appwrite tomorrow

--- app/(root)/(auth)/sign-in.tsx
+++ app/(root)/(auth)/sign-in.tsx
@@ -18,7 +18,7 @@ const SignIn = () => {
       <Text className="text-center mt-14 color-text-secondary font-josefinSans text-3xl ">Interesting articles for you{"\n"}</Text>
       <Text className='text-center color-text-secondary font-josefinSans text-3xl'>From all over the <Text className='font-bold'>internet</Text></Text>
 
-      <TouchableOpacity onPress={handleLogin} className='bg-white shadow-md shadow-zinc-300 rounded-full  py-4 mt-20 mx-6'>
+      <TouchableOpacity onPress={handleLogin} className='bg-white shadow-md shadow-zinc-300 rounded-full  py-4 mt-20 mx-20'>
         <View className='flex flex-row items-center justify-center'>
         <Image src={"https://cdn-teams-slug.flaticon.com/google.jpg"} className="w-9 h-9 mr-2" resizeMode='contain' />
           <Text className='text-xl font-montserrat font-bold'>Continue with Google</Text>
