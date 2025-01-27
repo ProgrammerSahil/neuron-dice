f1ed3171225b0f66588b82942c73ab354a37f857
Sahil Udar <udarsahil0@gmail.com>
2025-01-27 23:19:57 +0530

    the preferences page displays wikipedia data for now for testing

--- app/(root)/(tabs)/preferences.tsx
+++ app/(root)/(tabs)/preferences.tsx
@@ -1,12 +1,62 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchArticles } from '@/lib/wikipedia'
+import { AxiosResponse } from 'axios'
 
-const preferences = () => {
-  return (
-    <View>
-      <Text>preferences</Text>
-    </View>
-  )
+interface ArticlesState {
+  data?: any;
+  error?: string;
 }
 
-export default preferences
+const Preferences = () => {
+  const [articles, setArticles] = useState<ArticlesState>({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getArticles = async () => {
+      setIsLoading(true);
+      try {
+        const result = await fetchArticles(["tech", "politics"]);
+        if (result instanceof Error) {
+          setArticles({ error: result.message });
+        } else {
+          setArticles({ data: result.data });
+        }
+      } catch (error) {
+        setArticles({ error: 'Failed to fetch articles' });
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getArticles();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  if (articles.error) {
+    return (
+      <View>
+        <Text>Error: {articles.error}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <Text>{articles.data.query.search[0].title}</Text>
+      <Text>{articles.data.query.search[1].title}</Text>
+      <Text>{articles.data.query.search[2].title}</Text>
+      <Text>{articles.data.query.search[3].title}</Text>
+      <Text>{articles.data.query.search[4].title}</Text>
+    </View>
+  );
+}
+
+export default Preferences
--- lib/wikipedia.ts
+++ lib/wikipedia.ts
@@ -1,19 +1,18 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
-const fetchArticles = async (keywords: String[]) => {
+const fetchArticles = async (keywords: string[]): Promise<AxiosResponse | Error> => {
   try {
-    const query = keywords.join("|");
+    const query = keywords.join("+");
     const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${query}&utf8=1&origin=*`;
-
     const response = await axios.get(url);
-
     if (response.status === 200) {
       return response;
     } else {
       throw new Error("Failed to fetch articles");
     }
   } catch (e: any) {
-    return new Error("error occured while fetching wikipedia articles");
+    return new Error("Error occurred while fetching Wikipedia articles");
   }
 };
+
 export { fetchArticles };
