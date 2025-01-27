af079823aff138b52545b6177869b122e968faab
Sahil Udar <udarsahil0@gmail.com>
2025-01-27 22:47:03 +0530

    completed the tab nav

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,33 +1,62 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+
+const TabIcon = ({ focused, icon, title }: { focused: boolean; icon: string; title: string }) => (
+  <View>
+    <Image 
+      style={{ width: 30, height: 30, marginTop:25 }}  
+      source={{ uri: icon }}  
+      resizeMode="contain"
+      tintColor={focused? '#0061ff': '#666876'} 
+    />
+  </View>
+)
 
 const TabsLayout = () => {
   return (
     <Tabs
-        screenOptions={{
-            tabBarShowLabel:false,
-            tabBarStyle:{
-                backgroundColor: 'white',
-                position: 'absolute',
-                borderTopColor: '#0061FF1A',
-                borderTopWidth:1,
-                minHeight: 70,
-            }
-        }}
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: 'white',
+          position: 'absolute',
+          borderTopColor: '#0061FF1A',
+          borderTopWidth: 1,
+          minHeight: 70,
+        }
+      }}
     >
-
-      <Tabs.Screen name='index'
+      <Tabs.Screen 
+        name="index"
         options={{
-            title:'Home',
-            headerShown: false,
-            tabBarIcon: () => (
-                <View>
-                    <Text className='text-xs'>Home</Text>
-                </View>
-            )
+          title: 'Home',
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon 
+              icon="https://cdn-icons-png.flaticon.com/512/25/25694.png"
+              focused={focused}
+              title="home"
+            />
+          )
         }}
       />
+
+<Tabs.Screen 
+        name="preferences"
+        options={{
+          title: 'Preferences',
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon 
+              icon="https://upload.wikimedia.org/wikipedia/commons/d/dc/Settings-icon-symbol-vector.png"
+              focused={focused}
+              title="preferences"
+            />
+          )
+        }}
+      />
+
     </Tabs>
   )
 }
